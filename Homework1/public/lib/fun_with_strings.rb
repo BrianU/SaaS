ef6781dd643ed19497a348e587c41a4b05197d82
Brian Uniacke <brianuniacke@gmail.com>
2013-10-20 22:46:56 +0100

    All tests for HW1 passing

--- Homework1/public/lib/fun_with_strings.rb
+++ Homework1/public/lib/fun_with_strings.rb
@@ -1,4 +1,7 @@
 module FunWithStrings
+
+  @@anagram_groups = Hash.new
+
   def palindrome?
     return true if self.downcase.gsub(/[^0-9a-z]/i, '') == self.downcase.reverse.gsub(/[^0-9a-z]/i, '')
     return false
@@ -16,30 +19,32 @@ module FunWithStrings
   end
 
   def anagram_groups
-    words = Hash.new
+    my_array = Array.new
+    if self.empty?
+      return my_array
+    end
     self.lstrip.downcase.delete('^a-zA-Z ').split(/\s+(?=(?:[^"]*"[^"]*")*[^"]*$)/).each do |word|
-      word
-      words[word]=word
-          end
-    print words
-    return words
+      add_to_group( word )
+    end
+    @@anagram_groups.each_value do |value|
+      my_array.push(value.split(/,/))
+    end
+    return my_array
   end
 
-  def check_word_is_anagram?(words, word)
-
-    # to check if a word is an anagram of another
-    #1. convert the word into a list of letters
-    #2. compare the letters
-    #3. if they are the same then enter the group
-
+  def add_to_group( word )
+    new_group = {word.mince_word=>word}
+    return if word==nil
+    @@anagram_groups = new_group.merge(@@anagram_groups){|key, oldval, newval| "#{oldval},#{newval}"}
   end
-
-
+  def mince_word
+    return (self.split(//).sort).join
+  end
 end
+
 # make all the above functions available as instance methods on Strings:
 
 class String
   include FunWithStrings
 end
 
-"scream cars for four scar creams".anagram_groups
