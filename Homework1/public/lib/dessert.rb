faa6dde3157bcc5f78bd6a91dfd5c2effb5b4f85
Brian Uniacke <brianuniacke@gmail.com>
2013-10-21 00:26:05 +0100

    starting the rock paper scissorrs

--- Homework1/public/lib/dessert.rb
+++ Homework1/public/lib/dessert.rb
@@ -1,17 +1,29 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
+   @name     = name
+   @calories = calories
     # your code here
   end
   def healthy?
     # your code here
+    if self.calories < 200
+      return true
+    end
   end
   def delicious?
+    return true
     # your code here
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    super("#{flavor} jelly bean", 5)
+  end
+  def delicious?
+    return true unless flavor == 'licorice'
   end
 end
--- Homework1/public/lib/rock_paper_scissors.rb
+++ Homework1/public/lib/rock_paper_scissors.rb
@@ -5,10 +5,67 @@ class RockPaperScissors
 
   def self.winner(player1, player2)
     # YOUR CODE HERE
-  end
+    player1 = Player.new(player1)
+    player2 = Player.new(player2)
+    raise NoSuchStrategyError.new, 'Strategy must be one of R,P,S' unless
+        player1.has_legal_strategy? and player2.has_legal_strategy?
 
+    puts player1.fight(player2)
+
+
+  end
   def self.tournament_winner(tournament)
     # YOUR CODE HERE
   end
-
 end
+
+class Player
+  attr_accessor :name, :strategy
+  def initialize(player)
+    if player.empty?
+      return false
+    end
+    @strategy = player.pop
+    @name     = player.pop
+  end
+  def has_legal_strategy?
+    if strategy.downcase.match(/[rps]/)
+      return true
+    end
+    return false
+  end
+  def fight(enemy)
+    puts "Big fight #{self.name} versus #{enemy.name}"
+    if strategy.downcase == 'r'
+      if enemy.strategy == 'p'
+        return enemy
+      elsif enemy.strategy == 's'
+        return self
+      elsif enemy.strategy == 'r'
+        return self
+      end
+    end
+    if strategy.downcase == 'p'
+      if enemy.strategy == 'p'
+        return self
+      elsif enemy.strategy == 's'
+        return enemy
+      elsif enemy.strategy == 'r'
+        return self
+      end
+    end
+    if strategy.downcase == 's'
+      if enemy.strategy == 'p'
+        return self
+      elsif enemy.strategy == 's'
+        return self
+      elsif enemy.strategy == 'r'
+        return enemy
+      end
+    end
+  end
+end
+
+@rock = ['Armando','R'] ; @paper = ['Dave','P'] ; @scissors = ['Sam','S']
+
+RockPaperScissors.winner(@rock, @scissors)
