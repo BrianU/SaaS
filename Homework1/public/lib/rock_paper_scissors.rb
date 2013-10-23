ecff3c0e1b5d02037793ccde01679d4bd8f7643b
Brian Uniacke <brian.uniacke@smartbox.com>
2013-10-23 21:25:42 +0100

    Only the recursion failing

--- Homework1/public/lib/rock_paper_scissors.rb
+++ Homework1/public/lib/rock_paper_scissors.rb
@@ -6,69 +6,90 @@ class RockPaperScissors
   def self.winner(player1, player2)
     # YOUR CODE HERE
 
-    raise NoSuchStrategyError.new, 'Strategy must be one of R,P,S' unless
-        player1.has_legal_strategy? and player2.has_legal_strategy?
+#    raise NoSuchStrategyError.new, 'Strategy must be one of R,P,S' unless
+#        player1.has_legal_strategy? and player2.has_legal_strategy?
 
     winner =  player1.fight(player2)
+    if !winner
+      puts "Couldn't find a winner"
+      return [player1,player2]
+    end
     return winner
-
-
-
   end
 
   def self.tournament_winner(tournament)
+
     winners = []
+
     if tournament.size == 1
-      return tournament[0]
+      print "I've hit the bottom ... "
+      puts "the winner is ... "
+      print tournament
+      return tournament
+    else
+      tournament.each_slice(2) do |left, right|
+        winners <<  winner(left, right)
+   #     winners <<  winner(right[0],right[1])
+      end
     end
-    tournament.each_slice(2) do |left, right|
-      winners.push( winner(left, right) )
-    end
-    puts "interim winners ... "
-    print winners
+    puts "going deeper"
     tournament_winner( winners )
+  end
+end
+
+class String
+  def method_missing(method_id, *args, &block)
+    puts "whatever"
   end
 end
 
 
 
 module RPS
+
+  def method_missing( method_id, *args, &block )
+    puts "Hello Brian How are oyu? tried to call #{method_id} with these ars #{args}"
+  end
+
+
   def has_legal_strategy?
-    if self[1].to_s.match(/[rpsRPS]/)
+    if self[1].match(/[rpsRPS]/)
       return true
     end
     return false
   end
+
   def fight(enemy)
-    puts "Big fight #{self[0]} versus #{enemy[0]}"
     if self[1] == 'R'
       if enemy[1] == 'P'
-        return enemy
+        winner =enemy
       elsif enemy[1] == 'S'
-        return self
+        winner =self
       elsif enemy[1] == 'R'
-        return self
+        winner =self
       end
     end
     if self[1] == 'P'
       if enemy[1] == 'P'
-        return self
+        winner =self
       elsif enemy[1] == 'S'
-        return enemy
+        winner =enemy
       elsif enemy[1] == 'R'
-        return self
+        winner =self
       end
     end
     if self[1] == 'S'
       if enemy[1] == 'P'
-        return self
+        winner =self
       elsif enemy[1] == 'S'
-        return self
+        winner =self
       elsif enemy[1] == 'R'
-        return enemy
+        winner =enemy
       end
     end
+    return winner
   end
+
 end
 
 class Array
@@ -89,7 +110,7 @@ tourney = [
         ]
 
 
-puts RockPaperScissors.tournament_winner([@rock,@scissors])
+#puts RockPaperScissors.tournament_winner([@rock,@scissors])
 
-puts RockPaperScissors.tournament_winner( tourney )
+RockPaperScissors.tournament_winner( tourney )
 
